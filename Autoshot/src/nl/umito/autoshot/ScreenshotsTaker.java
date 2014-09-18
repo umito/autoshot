9084179be7aff2d19010250605ca98ed04b6d8b0
Peter <peterdekraker@umito.nl>
2014-09-18 20:38:13 +0200

    Add (non-compiling) example of the code I use in taking screenshots of my KeyChord app.

--- Autoshot/src/nl/umito/autoshot/ScreenshotsTaker.java
+++ Autoshot/src/nl/umito/autoshot/ScreenshotsTaker.java
@@ -3,6 +3,7 @@ package nl.umito.autoshot;
 import java.io.File;
 import java.io.IOException;
 import java.io.ObjectOutputStream.PutField;
+import java.util.ArrayList;
 import java.util.Locale;
 
 import org.apache.commons.io.FileUtils;
@@ -17,6 +18,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.content.res.Resources;
+import android.graphics.Rect;
 import android.os.Environment;
 import android.preference.PreferenceManager;
 import android.test.ActivityInstrumentationTestCase2;
@@ -24,6 +26,9 @@ import android.test.InstrumentationTestCase;
 import android.test.InstrumentationTestRunner;
 import android.util.DisplayMetrics;
 import android.util.Log;
+import android.view.View;
+import android.widget.HorizontalScrollView;
+import android.widget.ViewFlipper;
 
 /**
  * We use a ActivityInstrumentationTestCase2 here with the target activity as
@@ -155,4 +160,171 @@ public class ScreenshotsTaker extends
 		solo.sleep(1000);		
 		takeScreenshot("dashboard");
 	}
+	
+	/**
+	 * Example of using Robotium to move through keychord app and take screenshots
+	 * Won't compile ofcourse! 
+	 * @author Peter
+	 *
+	 */
+//	public void takeScreenshots() throws Throwable {
+//		resetScreenshotIndex();
+//	
+//		solo.sleep(1000);
+//		if (isLite)
+//		{
+//			solo.clickOnText(solo.getString(R.string.continue_button));
+//			solo.sleep(500);
+//		}
+//		takeScreenshot("dashboard");
+//
+//		// remove help messages
+//		CurrentData.setStaffHelpMessageShown(true);
+//		CurrentData.setReverseInfoMessageShown(true);
+//		CurrentData.setNormalInfoMessageShown(true);
+//		CurrentData.setScaleHelpMessageShown(true);
+//
+//		int[] dashboardButtons = { R.id.dashboard_chords_button,
+//				R.id.dashboard_scales_button,
+//				R.id.dashboard_staff_dictionary_button,
+//				R.id.dashboard_reverse_chords_button,
+//				R.id.dashboard_reverse_scales_button,
+//				R.id.dashboard_minipiano_button };
+//		String[] buttonNames = { "chords", "scales", "staff", "reverse_chords",
+//				"reverse_scales", "minipiano" };
+//		boolean[] hasSlidingDrawer = { true, true, true, false, false, false };
+//		for (int i = 0; i < dashboardButtons.length; i++) {
+//
+//			int id = dashboardButtons[i];
+//			if (isLite) {
+//				if (id == R.id.dashboard_staff_dictionary_button
+//						|| id == R.id.dashboard_reverse_chords_button) {
+//					continue;
+//				}
+//			}
+//			View button = solo.getView(id);
+//			solo.clickOnView(button);
+//			solo.sleep(1000);
+//
+//			String name = buttonNames[i];
+//			takeScreenshot(name + "_default");
+//			solo.sleep(1000);
+//			if (hasSlidingDrawer[i]) {
+//				View handle = solo.getView(R.id.ActionBar_Handle_Dots);
+//				View bottomItem = solo.getView(R.id.Statusbar);
+//				Log.d("sliding", handle.getX() + "," + handle.getX() + ","
+//						+ handle.getY() + "," + bottomItem.getY());
+//				solo.drag(handle.getX(), handle.getX(), handle.getY(),
+//						bottomItem.getY(), 10);
+//				solo.sleep(1000);
+//				takeScreenshot(name + "_default_slidingdrawer");
+//				solo.sleep(100);
+//
+//				switch (id) {
+//				case R.id.dashboard_chords_button: {
+//					solo.clickOnText("7");
+//					solo.sleep(1000);
+//					takeScreenshot(name + "_chord_selected");
+//					break;
+//				}
+//				case R.id.dashboard_scales_button: {
+//					solo.clickOnText("^Major");
+//					solo.sleep(1000);
+//					takeScreenshot(name + "_scale_selected");
+//					break;
+//				}
+//				case R.id.dashboard_staff_dictionary_button: {
+//					final ViewFlipper flipper = (ViewFlipper) solo
+//							.getView(R.id.drawer_viewflipper);
+//					runTestOnUiThread(new Runnable() {
+//
+//						@Override
+//						public void run() {
+//							flipper.setDisplayedChild(1);
+//
+//						}
+//					});
+//
+//					solo.clickOnText("-7");
+//					solo.sleep(1000);
+//					takeScreenshot(name + "_chord_selected");
+//
+//					solo.drag(handle.getX(), handle.getX(), handle.getY(),
+//							bottomItem.getY(), 10);
+//					runTestOnUiThread(new Runnable() {
+//
+//						@Override
+//						public void run() {
+//							flipper.setDisplayedChild(2);
+//
+//						}
+//					});
+//					solo.clickOnText("Blues");
+//					solo.sleep(1000);
+//					takeScreenshot(name + "_scale_selected");
+//					break;
+//				}
+//				}
+//			}
+//			switch (id) {
+//			case R.id.dashboard_reverse_chords_button: {
+//				final NoteSelectionPiano piano = (NoteSelectionPiano) solo
+//						.getView(R.id.NoteSelectionPiano);
+//				Note[] chord = { Note.parse("E4"), Note.parse("G#4"),
+//						Note.parse("B4"), Note.parse("D5") };
+//				HorizontalScrollView scrollView = (HorizontalScrollView) solo
+//						.getView(R.id.ScrollView);
+//				int scrollX = scrollView.getScrollX();
+//				for (Note note : chord) {//
+//
+//					Rect touchRect = piano.getKeyboard().getKey(note)
+//							.getTouchRegion().getBounds();
+//					int cx = touchRect.centerX();
+//					int cy = touchRect.centerY();
+//					int touchX = cx - scrollX;
+//					int touchY = (int) (scrollView.getY() + cy);
+//					solo.clickOnScreen(touchX, touchY);
+//					solo.sleep(25);
+//
+//				}
+//				solo.sleep(1000);
+//				takeScreenshot(name + "_chord_selected");
+//				break;
+//			}
+//			case R.id.dashboard_reverse_scales_button: {
+//				final NoteSelectionPiano piano = (NoteSelectionPiano) solo
+//						.getView(R.id.NoteSelectionPiano);
+//
+//				String[] noteNames = { "C4", "D4", "E4", "F#4", "G4", "A4",
+//						"B4" };
+//				ArrayList<Note> notes = new ArrayList<Note>();
+//				for (String noteName : noteNames) {
+//					notes.add(Note.parse(noteName));
+//				}
+//				HorizontalScrollView scrollView = (HorizontalScrollView) solo
+//						.getView(R.id.ScrollView);
+//				int scrollX = scrollView.getScrollX();
+//				for (Note note : notes) {//
+//
+//					Rect touchRect = piano.getKeyboard().getKey(note)
+//							.getTouchRegion().getBounds();
+//					int cx = touchRect.centerX();
+//					int cy = touchRect.centerY();
+//					int touchX = cx - scrollX;
+//					int touchY = (int) (scrollView.getY() + cy);
+//					solo.clickOnScreen(touchX, touchY);
+//					solo.sleep(25);
+//
+//				}
+//				solo.sleep(1000);
+//				takeScreenshot(name + "_scale_selected");
+//				break;
+//			}
+//			}
+//
+//			solo.goBack();
+//
+//		}		
+//	}
+	
 }
